8925e6617f154a5ab294bfb4a1596d0c072beaf3
Harshit joshi <harshjoshi25022002@gmail.com>
2026-06-23 11:30:05 +0530

    html page add

--- script.js
+++ script.js
@@ -14,15 +14,15 @@
 // })
 //timeline
 
-var tl = gsap.timeline()
+// var tl = gsap.timeline()
 
-tl.to("#box1",{
-    x:1500,
-    rotate:360,
-    duration:1.5,
-    delay:1
-})
-tl.to("#box2",{
-    x:1500,
-    duration:1.5
-})
+// tl.to("#box1",{
+//     x:1500,
+//     rotate:360,
+//     duration:1.5,
+//     delay:1
+// })
+// tl.to("#box2",{
+//     x:1500,
+//     duration:1.5
+// })
